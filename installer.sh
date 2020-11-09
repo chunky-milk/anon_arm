271b17f51e768ca8c828388860b559cfcf00a456
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2020-11-09 09:52:33 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Ensure we are being ran as root
+# Ensure script is being run as root
 if [ $(id -u) -ne 0 ]; then
-	echo "This script must be ran as root"
+	echo "This script must be run as root. Try again with sudo."
 	exit 1
 fi
 
@@ -16,20 +16,17 @@ apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
 rm /tmp/i2p-debian-repo.key.asc # delete the temp key
 apt-get update # Update repos
 
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
-fi
+apt-get install libservlet3.0-java 
+wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
+dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
+apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
+apt-get -f install # resolves anything else in a broken state
 
 apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
 apt-get install -y secure-delete tor i2p # install dependencies, just in case
 
 # Configure and install the .deb
-dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
-dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
+dpkg-deb -b anon_arm-deb-src/ anon-arm.deb # Build the deb package
+dpkg -i anon-arm.deb || (apt-get -f install && dpkg -i anon-arm.deb) # this will automatically install the required packages
 
 exit 0
